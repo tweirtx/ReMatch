e1147248b9d60f1d2e734923cd7d92c13079e9a3
Travis Weir <tweirtx@gmail.com>
2019-02-28 10:48:39 -0600

    More multi-day support, added test shortcut

--- ReMatch/__main__.py
+++ ReMatch/__main__.py
@@ -20,10 +20,31 @@ def main(video_id, archive_type,  event_key, event_type):
         print("Unsupported video type!")
         return exit(1)
     if event_type == 'frc':
-        TBA.DB_setup(TBA(), event_key, timestamp)
+        TBA.DB_setup(TBA(), event_key, timestamp, day2timestamp, day3timestamp)
     else:
         print("Unsupported event type!")
         return exit(1)
+    Splitter.split(Splitter(), event_key, event_type)
+
+def timestamp_and_dl(id, type):
+    if type == "twitch":
+        twitch_client = twitch.TwitchClient()
+        vodinf = twitch_client.videos.get_by_id(video_id)
+        ydl_opts = {
+            'format': 'best',
+        }
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([vodinf.get('url')])
+        return vodinf.get('created_at')
+    else:
+        return None
+
+def main_rewrite(event_key, event_type, day_one_id, day_one_type, day_two_id, day_two_type, day_three_id, day_three_type):
+    day_one_timestamp = timestamp_and_dl(day_one_id, day_one_type)
+    day_two_timestamp = timestamp_and_dl(day_two_id, day_two_type)
+    day_three_timestamp = timestamp_and_dl(day_three_id, day_two_type)
+    if event_type == 'frc':
+        TBA.DB_setup(TBA(), event_key, timestamp, day2timestamp, day3timestamp)
     Splitter.split(Splitter(), event_key, event_type)
 
 
@@ -40,6 +61,4 @@ if __name__ == '__main__':
     parser.add_argument('video_id_day_three', help="Optional argument for multiple day support", optional=True)
     parser.add_argument('video_type_day_three', help="Optional argument for multiple day support", optional=True)
     args = parser.parse_args().__dict__
-    print(args)
-    exit()
-    main(args['video_id'], args['video_type'], args['event_key'], args['event_type'])
+    main_rewrite(args['event_key'], args['event_type'], args['video_id_day_one'], args['video_type_day_one'], args['video_id_day_two'], args['video_type_day_two'], args['video_id_day_three'], args['video_type_day_three'])
---
+++ test.sh
@@ -0,0 +1,1 @@
+python3 -m ReMatch 237838649 twitch 2018txlu frc 237838520 twitch
