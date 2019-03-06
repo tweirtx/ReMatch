6d102dad965ab49a79e21cec984ff7f47dc79260
Travis Weir <tweirtx@gmail.com>
2019-03-06 12:05:53 -0600

    Probably working

--- ReMatch/splitter.py
+++ ReMatch/splitter.py
@@ -1,11 +1,13 @@
 from moviepy.editor import *
 import psycopg2
+import multiprocessing
 
 class Splitter:
     def split(self, event_key, event_type):
         db = psycopg2.connect(dbname="rematch", user="rematch", password="matchbox").cursor()
         db.execute("SELECT * FROM {};".format(event_type + event_key))
+        cores = multiprocessing.cpu_count()
         for match in db.fetchall():
-            video = VideoFileClip("{}{}.mp4".format(event_type + event_key + match['day']))
-            video.subclip(match["start_time"], match["start_time"] + 300)
-            video.write(match['match_key'] + ".mp4")
+            video = VideoFileClip("{}{}_{}.mp4".format(event_type, event_key, match[2]))
+            video.subclip(match[1], match[1] + 300)
+            video.write_videofile(match[0] + ".mp4", threads=cores)
