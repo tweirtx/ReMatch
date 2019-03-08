cef0ef564a73a495572fc74c5bccf29893e1f32b
Travis Weir <tweirtx@gmail.com>
2019-03-08 14:32:55 -0600

    Missed a flag

--- benchmark.py
+++ benchmark.py
@@ -3,6 +3,6 @@ import subprocess
 
 start_time = datetime.datetime.now().timestamp()
 
-subprocess.call("python3 -m ReMatch 389172543 twitch 2019txaus frc 389710248 twitch")
+subprocess.call("python3 -m ReMatch 389172543 twitch 2019txaus frc 389710248 twitch", shell=True)
 
 print("This benchmark run took {} seconds to complete".format(datetime.datetime.now().timestamp() - start_time))
