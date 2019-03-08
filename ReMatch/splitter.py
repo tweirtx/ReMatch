0377376b52011169d9648898e7047f343fe5b60d
Travis Weir <tweirtx@gmail.com>
2019-03-07 21:06:31 -0600

    Improved efficiency, and it works!

--- ReMatch/splitter.py
+++ ReMatch/splitter.py
@@ -1,6 +1,6 @@
-from moviepy.editor import *
 import psycopg2
 import multiprocessing
+import subprocess
 
 class Splitter:
     def split(self, event_key, event_type):
@@ -8,4 +8,6 @@ class Splitter:
         db.execute("SELECT * FROM {};".format(event_type + event_key))
         cores = multiprocessing.cpu_count()
         for match in db.fetchall():
-            video = VideoFileClip("{}{}_{}.mp4".format(event_type, event_key, match[2])).subclip(match[1], match[1] + 300).write_videofile(match[0] + ".mp4", threads=cores)
+#            video = VideoFileClip("{}{}_{}.mp4".format(event_type, event_key, match[2])).subclip(match[1], match[1] + 300).write_videofile(match[0] + ".mp4", threads=cores, ffmpeg_params="copy")
+            end_time = match[1] + 300
+            subprocess.call(f"ffmpeg -i {event_type}{event_key}_{match[2]}.mp4 -ss {match[1]} -to {end_time} -c copy {match[0]}.mp4", shell=True)
--- test.sh
+++ test.sh
@@ -1,3 +1,2 @@
- sudo -u postgres psql -f purge.sql
-# python3 -m ReMatch 237838649 twitch 2018txlu frc 237838520 twitch
+sudo -u postgres psql -f purge.sql
 python3 -m ReMatch 389172543 twitch 2019txaus frc 389710248 twitch
