b07d94ea81668e5a3245d1dcc52ae34463a3ad3e
Travis Weir <tweirtx@gmail.com>
2019-04-12 11:48:47 -0500

    Reduce package list

--- docker.sh
+++ docker.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-apt update && apt dist-upgrade -y && apt install ffmpeg curl postgresql-10 make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl git sudo python3-pip libpq-dev -y
+apt update && apt dist-upgrade -y && apt install ffmpeg postgresql-10 git sudo python3 -y
 git clone https://github.com/tweirtx/ReMatch
 python3 -m pip install -Ur ReMatch/requirements.txt
 adduser rematch
