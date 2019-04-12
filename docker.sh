8e4005f4450250e4efe7e9dc238187ceea3ed6c4
Travis Weir <tweirtx@gmail.com>
2019-04-12 12:29:36 -0500

    Have a package

--- docker.sh
+++ docker.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-apt update && apt dist-upgrade -y && apt install ffmpeg postgresql-10 git sudo python3 -y
+apt update && apt dist-upgrade -y && apt install ffmpeg postgresql-10 git sudo python3 python3-pip -y
 git clone https://github.com/tweirtx/ReMatch
 python3 -m pip install -Ur ReMatch/requirements.txt
 adduser rematch --disabled-password --gecos ""
