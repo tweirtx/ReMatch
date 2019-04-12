40f30c62c461c485c5a8fd5dbf9a62fcf11bc0a9
Travis Weir <tweirtx@gmail.com>
2019-04-12 11:59:29 -0500

    Noninteractive user adding/proper directory

--- docker.sh
+++ docker.sh
@@ -2,6 +2,6 @@
 apt update && apt dist-upgrade -y && apt install ffmpeg postgresql-10 git sudo python3 -y
 git clone https://github.com/tweirtx/ReMatch
 python3 -m pip install -Ur ReMatch/requirements.txt
-adduser rematch
+adduser rematch --disabled-password --gecos
 sudo service postgresql restart
 chmod 777 ReMatch
