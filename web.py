791084a5b65fd97bb9c85670d523965b15a2722a
Travis Weir <tweirtx@gmail.com>
2019-04-12 12:45:18 -0500

    Change some code

--- web.py
+++ web.py
@@ -1,4 +1,5 @@
 import subprocess
+import socket
 from flask import request, Flask, send_from_directory
 
 app = Flask("ReMatch-Web")
@@ -39,5 +40,18 @@ def set_tba_key():
     return "Key set successfully!"
 
 
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host=get_ip())
