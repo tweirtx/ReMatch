bedd85fd5ae2176128fac738cf95e7d65a127639
Travis Weir <tweirtx@gmail.com>
2019-04-22 12:11:24 -0500

    Better scripting

--- docker_up.ps1
+++ docker_up.ps1
@@ -1,5 +1,4 @@
-#!/usr/bin/env bash
 docker pull tweirtx/rematch
 docker container rename $(docker run -d -p 5000:5000 tweirtx/rematch) rematch
-sleep 5
-echo "Container is now up, and can be accessed at http://localhost:5000"
+Start-Sleep 5
+Write-Output "Container is now up, and can be accessed at http://localhost:5000"
