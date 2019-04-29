ea2032f73bd4e3686544d7ccd738895a44f3e2c0
Travis Weir <tweirtx@gmail.com>
2019-04-29 12:37:42 -0500

    Removed obsolete method

--- web.py
+++ web.py
@@ -125,18 +125,5 @@ def oauth2callback():
     return redirect(url_for('auth'))
 
 
-def get_ip():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    try:
-        # doesn't even have to be reachable
-        s.connect(('10.255.255.255', 1))
-        IP = s.getsockname()[0]
-    except:
-        IP = '127.0.0.1'
-    finally:
-        s.close()
-    return IP
-
-
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
